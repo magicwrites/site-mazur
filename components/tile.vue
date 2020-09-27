f86143557963abe420fb13cadb892b45ce893b49
Maciej Mazur <maciej@mazur.site>
2020-09-28 00:03:32 +0200

    initial rework

---
+++ components/tile.vue
@@ -0,0 +1,181 @@
+<template>
+  <div class="tile">
+    <div class="tile-small">
+      <mazur-tile>
+        <mazur-avatar :size="88" :url="avatar" class="bordered"></mazur-avatar>
+        <i v-if="icon" class="fas fa-baby-carriage icon"></i>
+        <div style="margin-left: -1px; margin-top: -3px">
+          <mazur-text class="big normal block" style="margin-left: -1.5px">{{
+            name
+          }}</mazur-text>
+          <mazur-space class="smallest negative"></mazur-space>
+          <mazur-text class="smaller muted montserrat block">Mazur</mazur-text>
+        </div>
+        <mazur-space class="smallest"></mazur-space>
+        <span v-for="(element, index) in about" :key="index">
+          <mazur-text v-if="index != 0" class="smaller nunito muted">
+            /</mazur-text
+          >
+          <mazur-text class="smaller nunito">{{ element }}</mazur-text>
+        </span>
+        <mazur-space class="blank"></mazur-space>
+        <a v-if="link" v-bind:href="link" target="_blank">
+          <mazur-link class="smaller"
+            >{{ isEnglish ? 'meet' : 'poznaj' }} »</mazur-link
+          >
+        </a>
+      </mazur-tile>
+    </div>
+    <div class="tile-large">
+      <mazur-tile>
+        <mazur-avatar :size="120" :url="avatar" class="bordered"></mazur-avatar>
+        <mazur-space class="smallest"></mazur-space>
+        <i v-if="icon" class="fas fa-baby-carriage icon"></i>
+        <div style="margin-left: -1px; margin-top: -3px">
+          <mazur-text class="big normal block" style="margin-left: -1.5px"
+            >{{ name }} Mazur</mazur-text
+          >
+        </div>
+        <mazur-space class="smaller"></mazur-space>
+        <span v-for="(element, index) in about" :key="index">
+          <mazur-text v-if="index != 0" class="small muted separator">
+            /</mazur-text
+          >
+          <mazur-text class="small">{{ element }}</mazur-text>
+        </span>
+        <mazur-space class="small"></mazur-space>
+        <a v-if="link" v-bind:href="link" target="_blank">
+          <mazur-link class="small"
+            >{{ isEnglish ? 'meet' : 'poznaj' }} »</mazur-link
+          >
+        </a>
+      </mazur-tile>
+    </div>
+  </div>
+</template>
+
+<script>
+import {
+  MazurTile,
+  MazurText,
+  MazurSpace,
+  MazurLink,
+  MazurAvatar,
+} from '@maciejmazur/core-components'
+
+export default {
+  components: {
+    MazurTile,
+    MazurText,
+    MazurSpace,
+    MazurLink,
+    MazurAvatar,
+  },
+  props: {
+    avatar: { type: String, default: null },
+    name: { type: String, default: null },
+    about: { type: Array, default: () => [] },
+    icon: { type: String, default: null },
+    link: { type: String, default: null },
+    isEnglish: { type: Boolean, default: false },
+  },
+}
+</script>
+
+<style scoped>
+.tile-small {
+  margin-top: 0;
+  width: 340px;
+}
+
+.tile-small .icon {
+  float: right;
+  margin-top: 4px;
+  margin-right: 4px;
+}
+
+.tile-small .mazur-tile {
+  padding: 20px;
+  min-height: 115px;
+  position: relative;
+  padding-left: 115px;
+}
+
+.tile-small .mazur-avatar {
+  position: absolute;
+  top: 16px;
+  left: 16px;
+}
+
+.tile-large {
+  padding-top: 60px;
+  width: 510px;
+  position: relative;
+}
+
+.tile-large .mazur-tile {
+  height: 200px;
+  padding-top: 70px;
+  padding-left: 10px;
+  text-align: center;
+}
+
+.tile-large .separator {
+  padding: 0 10px;
+}
+
+.tile-large .mazur-avatar {
+  position: absolute;
+  top: 60px;
+  left: 50%;
+  margin: -59px 0 0 -59px;
+}
+
+@media (max-width: 1200px) {
+  .tile-large {
+    display: none;
+  }
+}
+
+@media (min-width: 1201px) {
+  .tile-small {
+    display: none;
+  }
+
+  .small .tile-small {
+    display: block;
+  }
+
+  .small .tile-large {
+    display: none;
+  }
+}
+
+/* .tile.small .mazur-tile {
+  width: 300px;
+} */
+
+/* .tile {
+  margin: 10px;
+  padding-top: 60px;
+}
+
+.tile .mazur-tile {
+  padding: 20px;
+  width: 340px;
+  position: relative;
+  padding-top: 75px;
+  text-align: center;
+}
+
+.tile .mazur-avatar {
+  position: absolute;
+  top: 0;
+  left: 50%;
+  margin: -59px 0 0 -59px;
+}
+
+.tile.small .mazur-tile {
+  width: 300px;
+} */
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,182 @@
+<template>
+  <div class="homepage">
+    <div class="header">
+      <div class="toast" style="opacity: 0">
+        <mazur-text class="smaller block white nunito hide-on-lg"
+          >&nbsp;</mazur-text
+        >
+      </div>
+      <div class="logo-centerer">
+        <mazur-space class="big show-on-md"></mazur-space>
+        <!-- <logo v-bind:reversed="true"></logo> -->
+        <mazur-logo v-bind:reversed="true"></mazur-logo>
+        <mazur-space></mazur-space>
+        <mazur-space class="hide-on-lg show-on-md"></mazur-space>
+      </div>
+    </div>
+    <div class="contents">
+      <div class="content">
+        <mazur-space class="smaller hide-on-md"></mazur-space>
+        <tile
+          name="Marcelina"
+          avatar="http://mazur.site/assets/1.0.0/images/avatars/marcelina-256.png"
+          link="http://marcelina.mazur.site"
+          :size="110"
+          :isEnglish="isEnglish"
+          :about="
+            isEnglish
+              ? ['accounting', 'liturgical music']
+              : ['księgowość', 'muzyka liturgiczna']
+          "
+        ></tile>
+        <mazur-space class="smaller hide-on-lg"></mazur-space>
+        <tile
+          name="Maciej"
+          avatar="https://s.gravatar.com/avatar/d68f29b47d8ba2af9ca67fc723fe6907"
+          link="http://maciej.mazur.site"
+          :size="110"
+          :isEnglish="isEnglish"
+          :about="
+            isEnglish ? ['software', 'design'] : ['oprogramowanie', 'design']
+          "
+        ></tile>
+      </div>
+      <mazur-space></mazur-space>
+      <div class="content">
+        <tile
+          class="small"
+          name="Helena"
+          icon="fas fa-baby"
+          avatar="https://s.gravatar.com/avatar/7817cdf448989cbc0540b023551f547d?s=120"
+          link="https://photos.app.goo.gl/ZBzunxbE2Xnv5SG27"
+          :isEnglish="isEnglish"
+          :about="
+            isEnglish
+              ? ['growing', 'attention seeking']
+              : ['jedzenie', 'poszukiwanie uwagi']
+          "
+        ></tile>
+      </div>
+    </div>
+    <div class="expand footer">
+      <mazur-space class="small hide-on-lg"></mazur-space>
+      <div class="toast">
+        <mazur-text class="smaller block white nunito"
+          >{{ isEnglish ? 'Przełączyć na' : 'Would you rather' }}
+          <mazur-link class="smaller" v-on:click.native="toggleLanguage">{{
+            isEnglish ? 'Język Polski' : 'switch to English'
+          }}</mazur-link
+          >?</mazur-text
+        >
+      </div>
+      <!-- <mazur-space class="smaller hide-on-xs"></mazur-space> -->
+    </div>
+  </div>
+</template>
+
+<script>
+import {
+  MazurSpace,
+  MazurText,
+  MazurLink,
+  MazurLogo,
+} from '@maciejmazur/core-components'
+// import Logo from '~/components/logo'
+import Tile from '~/components/tile'
+
+export default {
+  components: {
+    Tile, // Logo,
+    MazurLink,
+    MazurSpace,
+    MazurText,
+    MazurLogo,
+  },
+  data: () => {
+    const language = process.client
+      ? navigator.language || window.navigator.userLanguage
+      : 'en'
+
+    return {
+      isEnglish: language !== 'pl',
+    }
+  },
+  methods: {
+    toggleLanguage() {
+      console.log('toggle!')
+      this.isEnglish = !this.isEnglish
+    },
+  },
+}
+</script>
+
+<style>
+.homepage {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.header {
+  display: flex;
+  flex-direction: column;
+}
+
+.expand {
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+}
+
+.footer {
+  justify-content: flex-end;
+  align-items: center;
+}
+
+.toast {
+  padding: 15px 0;
+}
+
+.white {
+  color: white;
+}
+
+.mazur-logo {
+  height: 100px;
+}
+
+.logo-centerer {
+  margin: 0 auto;
+  /* position: absolute;
+  left: 50%;
+  margin-left: -75px;
+  transform: translateY(100px); */
+}
+
+@media (min-width: 801px) {
+  .contents {
+    min-width: 690px;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .content {
+    min-width: 690px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+}
+
+@media (min-width: 1201px) {
+  .contents,
+  .content {
+    min-width: 1060px;
+  }
+
+  .content:nth-child(3) {
+    justify-content: center;
+  }
+}
+</style>
