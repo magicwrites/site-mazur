1582adf45c561c64ea450d7691e1cb47ff1ef7d8
Maciej Mazur <maciej@mazur.site>
2022-11-14 13:25:54 +0100

    nice log and redirect code 308

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,12 +1,13 @@
 import { redirect } from '@sveltejs/kit';
 
 const subdomains = ['maciej', 'marcelina', 'helena', 'mateusz'];
+const PERMANENT_REDIRECT_CODE = 308;
 
 export const load = ({ url }) => {
 	const { host } = url;
 	const subdomain = subdomains.find((subdomain) => host.includes(subdomain));
 
 	if (subdomain) {
-		throw redirect(307, `https://www.mazur.site/${subdomain}`);
+		throw redirect(PERMANENT_REDIRECT_CODE, `https://www.mazur.site/${subdomain}`);
 	}
 };
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,11 @@
 <script>
 	import '../app.css';
 	import '../fonts.css';
+
+	console.info(`Hello inquisitive human being!
+
+If you would like to know more about my code then visit https://github.com/magicwrites/site-mazur
+The repository is open, so you should be able to check how I have cooked this thing up.`);
 </script>
 
 <slot />
