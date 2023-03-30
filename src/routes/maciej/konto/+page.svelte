d9ec549d2f42d933548d83435f71eccffba69b80
Maciej Mazur <maciej@mazur.site>
2023-03-30 13:00:21 +0200

    accounts updated

--- src/routes/maciej/konto/+page.svelte
+++ src/routes/maciej/konto/+page.svelte
@@ -7,8 +7,53 @@
 
 	// $: texts = $isPolish ? pl : en;
 
-	let eur = 'PL 85 1050 1618 1000 0097 0465 6108';
-	let pln = '47 1050 1618 1000 0023 2118 9355';
+	const accounts = {
+		personal: {
+			eur: 'PL 85 1050 1618 1000 0097 0465 6108',
+			pln: '47 1050 1618 1000 0023 2118 9355'
+		},
+		company: {
+			eur: 'PL 35 1050 1298 1000 0097 8036 1680',
+			pln: '52 1050 1298 1000 0090 8214 2861'
+		}
+	};
+
+	const personal = [
+		{
+			id: Symbol(),
+			title: 'EURO',
+			number: accounts.personal.eur
+		},
+		{
+			id: Symbol(),
+			title: 'PLN',
+			number: accounts.personal.pln
+		}
+	];
+
+	const company = [
+		{
+			id: Symbol(),
+			title: 'EURO',
+			number: accounts.company.eur
+		},
+		{
+			id: Symbol(),
+			title: 'PLN',
+			number: accounts.company.pln
+		}
+	];
+
+	const entries = [
+		{
+			title: 'prywatne',
+			accounts: personal
+		},
+		{
+			title: 'firmowe',
+			accounts: company
+		}
+	];
 
 	$: copied = null;
 </script>
@@ -18,62 +63,64 @@
 </svelte:head>
 
 <section class="flex h-screen font-work">
-	<section class="m-auto flex flex-col gap-y-10">
-		<div class="fixed top-0 left-0">
-			<a class="p-4 mb-20 flex items-center text-gray-400 transition hover:text-gray-700" href="/">
+	<section class="m-auto flex flex-col xl:gap-y-10">
+		<div class="xl:fixed top-0 left-0">
+			<a
+				class="p-4 mb-8 xl:mb-20 flex items-center text-gray-400 transition hover:text-gray-700"
+				href="/"
+			>
 				<i class="fa-solid fa-arrow-left text-gray-700 pr-3" />
 				<span class="text-sm font-work">na stronę główną</span>
 			</a>
 		</div>
 
-		<section>
-			<div class="px-4 pb-2 text-gray-400">Środki w EURO</div>
-			<div class="px-4 text-4xl -ml-0.5">
-				{eur}
-			</div>
-
-			<section class="px-4 mt-3 flex justify-end items-center gap-x-4 text-sm">
-				{#if copied === 'eur'}
-					<div transition:slide class="mr-auto sm:mr-0">
-						<span>skopiowano</span>
-						<i class="fa-solid fa-check" />
+		<main class="grid gap-16 px-4 pb-4">
+			{#each entries as entry}
+				<div class="flex flex-col gap-4">
+					<div class="text-2xl">
+						<span>Przelewy</span>
+						<span class="border-b border-neutral-700">{entry.title}</span>
 					</div>
-				{/if}
-				<Clipboard text={eur.replaceAll(' ', '')} let:copy on:copy={() => (copied = 'eur')}>
-					<button
-						class="border border-gray-400 hover:border-gray-700 rounded text-gray-400 hover:text-gray-700 transition"
-						on:click={copy}
-					>
-						<div class="cursor-pointer p-2 px-4 flex items-center">
-							<span>kopiuj do schowka</span>
-						</div>
-					</button>
-				</Clipboard>
-			</section>
-		</section>
 
-		<section>
-			<div class="px-4 pb-2 text-gray-400">Środki w PLN</div>
-			<div class="px-4 text-4xl -ml-0.5">{pln}</div>
+					<div class="flex flex-col gap-12">
+						{#each entry.accounts as account}
+							<section class="flex flex-col gap-2">
+								<header class="text-2xl font-light">
+									{account.number}
+								</header>
 
-			<section class="px-4 mt-3 flex justify-end items-center gap-x-4 text-sm">
-				{#if copied === 'pln'}
-					<div transition:slide class="mr-auto sm:mr-0">
-						<span>skopiowano</span>
-						<i class="fa-solid fa-check" />
+								<footer class="xl:flex justify-between">
+									<div class="text-gray-400">{account.title}</div>
+
+									<section class="flex justify-end items-center gap-x-4 text-sm">
+										{#if copied === account.id}
+											<div transition:slide class="mr-auto sm:mr-0">
+												<span>skopiowano</span>
+												<i class="fa-solid fa-check" />
+											</div>
+										{/if}
+
+										<Clipboard
+											text={account.number.replaceAll(' ', '')}
+											let:copy
+											on:copy={() => (copied = account.id)}
+										>
+											<button
+												class="px-2 border border-gray-400 hover:border-gray-700 rounded text-gray-400 hover:text-gray-700 transition"
+												on:click={copy}
+											>
+												<div class="cursor-pointer p-2 flex items-center">
+													<span>kopiuj do schowka</span>
+												</div>
+											</button>
+										</Clipboard>
+									</section>
+								</footer>
+							</section>
+						{/each}
 					</div>
-				{/if}
-				<Clipboard text={pln.replaceAll(' ', '')} let:copy on:copy={() => (copied = 'pln')}>
-					<button
-						class="border border-gray-400 hover:border-gray-700 rounded text-gray-400 hover:text-gray-700 transition"
-						on:click={copy}
-					>
-						<div class="cursor-pointer p-2 px-4 flex items-center">
-							<span class="text-sm">kopiuj do schowka</span>
-						</div>
-					</button>
-				</Clipboard>
-			</section>
-		</section>
+				</div>
+			{/each}
+		</main>
 	</section>
 </section>
