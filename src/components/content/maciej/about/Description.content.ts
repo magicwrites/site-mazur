7eebc9678b6b1cb6c24b71a047b417e3d99be805
Maciej Mazur <maciej@mazur.site>
2023-04-26 08:40:47 +0200

    better highlight and typo

--- src/components/content/maciej/about/Description.content.ts
+++ src/components/content/maciej/about/Description.content.ts
@@ -1,6 +1,6 @@
 export const pl = {
 	paragraphs: [
-		'<a href="http://cleancoder.com" target="_blank" class="border-b border-transparent hover:border-gray-400 transition">Tworzę</a> wizualną, interaktywną część aplikacji internetowych. Robię to indywidualnie, w ramach koleżeńskich zespołów start-up\'owych oraz w formalnych środowiskach korporacyjnych operujących w skali globalnej.',
+		"Tworzę wizualną, interaktywną część aplikacji internetowych. Robię to indywidualnie, w ramach koleżeńskich zespołów start-up'owych oraz w formalnych środowiskach korporacyjnych operujących w skali globalnej.",
 		'Jestem doświadczonym programistą, który rozumie jak osiągać rezultaty i lubi swoją pracę. Mogę odgrywać wiele ról w procesie tworzenia oprogramowania ale najlepiej odnajduję się jako front-end&nbsp;/&nbsp;JS deweloper. Zachęcam do przejrzenia mojego <a href="#experience" class="border-b border-neutral-700">doświadczenia</a> oraz <a href="#skillset" class="border-b border-neutral-700">umiejętności</a>&nbsp;-&nbsp;być może to właśnie mnie szukasz do swojego nowego projektu.'
 	]
 };
