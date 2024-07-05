478e50606147c665ebcc0f8c405a363a3cd3cd66
Maciej Mazur <maciej@mazur.site>
2024-07-05 16:38:35 +0200

    new presents

--- src/routes/maciej/prezent/+page.svelte
+++ src/routes/maciej/prezent/+page.svelte
@@ -16,6 +16,133 @@
 			</header>
 			<main class="flex flex-col gap-6">
 				<header>
+					<div class="text-lg -ml-0.25">Terrarystyka</div>
+					<div class="text-sm text-gray-400">Rośliny, słoiki, materiały i podobne.</div>
+				</header>
+				<div class="flex flex-col gap-2">
+					<Item>
+						<a
+							href="https://bioterr.pl/pl/p/BioTweezer-Angled-Peseta-profesjonalna-zakrzywiona/663"
+							target="_blank"
+							class="underline"
+						>
+							<span>pęseta zakrzywiona 31cm</span>
+						</a>
+						<span>41 zł</span>
+					</Item>
+					<Item>
+						<a
+							href="https://bioterr.pl/pl/p/BioTweezer-Straight-Peseta-profesjonalna-prosta/662"
+							target="_blank"
+							class="underline"
+						>
+							<span>pęseta prosta 31cm</span>
+						</a>
+						<span>41 zł</span>
+					</Item>
+					<Item>
+						<a
+							href="https://allegro.pl/oferta/sloik-na-ogorki-4l-pojemnik-ze-szklana-pokrywka-na-zywnosc-kawe-ciacho-15743859932"
+							target="_blank"
+							class="underline"
+						>
+							<span>dwa słoiki 4 litrowe</span>
+						</a>
+						<span>{36 * 2} zł</span>
+					</Item>
+					<Item>
+						<a
+							href="https://allegro.pl/oferta/sloiki-na-zywnosc-2l-na-ogorki-szklany-z-pokrywka-ciacho-zestaw-2-sztuk-15823914497"
+							target="_blank"
+							class="underline"
+						>
+							<span>dwa słoiki 2 litrowowe</span>
+						</a>
+						<span>56 zł</span>
+					</Item>
+					<Item>
+						<a
+							href="https://allegro.pl/oferta/sloiki-szklane-na-ogorki-kawe-1l-pojemnik-z-pokrywka-ciacho-zestaw-2-sztuk-15823914439"
+							target="_blank"
+							class="underline"
+						>
+							<span>dwa słoiki 1 litrowe</span>
+						</a>
+						<span>49 zł</span>
+					</Item>
+					<Item>
+						<a
+							href="https://allegro.pl/oferta/worm-castings-nawoz-od-dzdzownic-bio-11481862200"
+							target="_blank"
+							class="underline"
+						>
+							<span>2 litry nawozu od dżdżownic</span>
+						</a>
+						<span>14 zł</span>
+					</Item>
+					<Item>
+						<a
+							href="https://allegro.pl/oferta/lava-wulkaniczna-hydroponika-czarna-12027129785"
+							target="_blank"
+							class="underline"
+						>
+							<span>2 opakowania lawy wulkanicznej czarnej</span>
+						</a>
+						<span>25 zł</span>
+					</Item>
+					<Item>
+						<a
+							href="https://allegro.pl/oferta/worm-castings-nawoz-od-dzdzownic-bio-11481862200"
+							target="_blank"
+							class="underline"
+						>
+							<span>2 opakowania lawy wulkaniczej brązowej</span>
+						</a>
+						<span>20 zł</span>
+					</Item>
+					<Item>
+						<a
+							href="https://bioterr.pl/pl/p/Ficus-pumila-Fikus-pnacy-Zywa-Roslina-do-Terrarium/1795"
+							target="_blank"
+							class="underline"
+						>
+							<span>sadzonka fikusa do terrarium</span>
+						</a>
+						<span>25 zł</span>
+					</Item>
+					<Item>
+						<a
+							href="https://bioterr.pl/pl/p/Peperomia-hope-hybryda-quadrifolia-x-deppeana-Zywa-Roslina-do-Terrarium/2511"
+							target="_blank"
+							class="underline"
+						>
+							<span>sadzonka peperomia do terrarium</span>
+						</a>
+						<span>40 zł</span>
+					</Item>
+					<Item>
+						<a
+							href="https://bioterr.pl/pl/p/Pilea-mollis-Moon-Valley-Zywa-Roslina-do-Terrarium/2514"
+							target="_blank"
+							class="underline"
+						>
+							<span>sadzonka pilea mollis do terrarium</span>
+						</a>
+						<span>25 zł</span>
+					</Item>
+					<Item>
+						<a
+							href="https://bioterr.pl/pl/p/Pilea-cadierei-Pilea-cadiera-Zywa-Roslina-do-Terrarium/2513"
+							target="_blank"
+							class="underline"
+						>
+							<span>sadzonka pilea cadierei do terrarium</span>
+						</a>
+						<span>25 zł</span>
+					</Item>
+				</div>
+
+				<header>
 					<div class="text-lg -ml-0.25">Test DNA</div>
 					<div class="text-sm text-gray-400">Szczegóły na stronie internetowej.</div>
 				</header>
@@ -27,7 +154,7 @@
 					</Item>
 				</div>
 
-				<header>
+				<!-- <header>
 					<div class="text-lg -ml-0.25">Książkę</div>
 					<div class="text-sm text-gray-400">
 						Kolejność nieprzypadkowa, mile widziane twarde eleganckie oprawy, może być po angielsku
@@ -97,9 +224,9 @@
 						<span>Kontra</span>
 						<span class="text-gray-400">- Józef Mackiewicz</span>
 					</Item>
-				</div>
+				</div> -->
 
-				<header>
+				<!-- <header>
 					<div class="text-lg -ml-0.25">Edukacja</div>
 					<div class="text-sm text-gray-400">Zabawki do nauki.</div>
 				</header>
@@ -114,7 +241,7 @@
 						</a>
 						<span class="text-gray-400">VT EDUBOX4</span>
 					</Item>
-				</div>
+				</div> -->
 			</main>
 		</section>
 	</div>
