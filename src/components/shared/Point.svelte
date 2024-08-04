ea7ade4cb2e3773306cf82153f7ebf903a840908
Maciej Mazur <maciej@mazur.site>
2024-08-04 18:54:34 +0200

    alignment

--- src/components/shared/Point.svelte
+++ src/components/shared/Point.svelte
@@ -1,17 +1,17 @@
 <script>
-	export let first = false;
+  export let first = false;
 </script>
 
 <div class="flex flex-col sm:flex-row print:flex-row items-center sm:items-start">
-	<div class="py-2 sm:py-0 print:py-0 sm:mr-4 print:mr-8">
-		<div class={first ? 'hidden sm:block print:block' : ''}>
-			<div class="sm:hidden py-2">
-				<i class="fas fa-circle text-xs" />
-			</div>
-			<div class="hidden sm:block">
-				<i class="text-sm fas fa-circle rotate-90 sm:rotate-0 print:rotate-0" />
-			</div>
-		</div>
-	</div>
-	<div><slot /></div>
+  <div class="py-2 sm:py-0 print:py-0 sm:mr-4 print:mr-8">
+    <div class={first ? 'hidden sm:block print:block' : ''}>
+      <div class="sm:hidden py-2">
+        <i class="fas fa-circle text-xs" />
+      </div>
+      <div class="hidden sm:block">
+        <i class="text-sm fas fa-circle rotate-90 sm:rotate-0 print:rotate-0" />
+      </div>
+    </div>
+  </div>
+  <div class="w-full"><slot /></div>
 </div>
