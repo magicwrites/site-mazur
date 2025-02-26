dd8d31b999eeda18483e2a4b1fb35639884e0af4
Maciej Mazur <maciej@mazur.site>
2025-02-26 20:20:59 +0100

    privacy policy

---
+++ src/routes/ostatnie-ciacho/prywatnosc/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import Sheet from '$src/components/Sheet.svelte';
+</script>
+
+<section class="p-4 py-16">
+  <Sheet>
+    <div class="flex flex-col gap-8">
+      <h1 class="text-3xl">Polityka prywatności</h1>
+
+      <div class="flex flex-col gap-4">
+        <p>
+          Aplikacja <q class="font-medium">Ostatnie Ciacho</q> nie zbiera, nie przechowuje ani nie udostępnia
+          żadnych danych użytkownika. Wszystkie dane pozostają wyłącznie na urządzeniu użytkownika i
+          nie są przesyłane na żadne serwery zewnętrzne.
+        </p>
+
+        <p>W przypadku pytań dotyczących prywatności, skontaktuj się z nami pod adresem:</p>
+      </div>
+
+      <span class="text-center font-medium">administrator@mazur.site</span>
+    </div>
+  </Sheet>
+</section>
